68407dc51edc334bf6f7671d6a80b49e0c650cf7
NikosGour <nikosgournak@gmail.com>
2024-03-23 07:59:40 +0200

    Latest

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,38 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
 
 app.use(cors());
 
-app.get(`/recommend`, (req, res) =>
+app.get(`/recommend/:user_id`, (req, res) =>
 {
-	res.send(`Hello Testing!`);
+	const user_id = req.params.user_id;
+	if (user_id === ``)
+	{
+		res.status(400).send(`User ID is required`);
+		return;
+	}
+	// TODO: Test if the following code is correct
+	if (isNaN(Number(user_id)))
+	{
+		res.status(400).send(`User ID must be a number`);
+		return;
+	}
+	// TODO: add more guards
+	// TODO: Implement the recommendation logic
+
+
+
+	res.send(`Hello ${user_id}!`);
 });
 
-app.listen(3000, () =>
+
+const port = process.env[ `PORT` ] || 3000;
+app.listen(port, () =>
 {
-	console.log(`Server is running on http://localhost:3000`);
+	console.log(`Server is running on http://localhost:${port}`);
 });
