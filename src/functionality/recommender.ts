f74f4bb2322d8188e88c4a0800227b6a78d7c03c
NikosGour <nikosgournak@gmail.com>
2024-04-15 11:42:51 +0300

    Latest

--- src/functionality/recommender.ts
+++ src/functionality/recommender.ts
@@ -1,4 +1,12 @@
-export class Recommender
+export abstract class Recommender
 {
+	private static recommendation_engine: RecommendationEngine;
 
+
+	public static set_recommender_method(recommendation_engine: RecommendationEngine): void
+	{
+
+	}
 }
+
+export type RecommendationEngine = string;
